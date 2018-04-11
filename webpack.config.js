49ee1b10ad6d224847d4e9bf22070ee1b5f0dcc4
JMJ90 <jakemjohnson90@gmail.com>
2018-04-11 11:07:05 -0500

    changed webpack config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
+const LiveReloadPlugin = require('webpack-livereload-plugin')
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  mode: isDev ? 'development' : 'production',
-  entry: [
-    '@babel/polyfill', // enables async-await
-    './client/index.js'
-  ],
+  entry: ['babel-polyfill', './client/index.js'],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -15,9 +12,13 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader'
       }
     ]
-  }
+  },
+  // When we're in development, we can use this handy live-reload plugin
+  // to refresh the page for us every time we make a change to our client-side
+  // files. It's like `nodemon` for the front end!
+  plugins: isDev ? [new LiveReloadPlugin({appendScriptTag: true})] : []
 }
