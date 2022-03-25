051d20d1a9a5972b120e55ae0351a7eeb2474c24
Jake Johnson <jakej.dev@gmail.com>
2022-03-24 22:55:43 -0500

    update

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,22 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
+const Sequelize = require("sequelize");
+const pkg = require("../../package.json");
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
+let DATABASE_URL;
+process.env.NODE_ENV === "development"
+  ? (DATABASE_URL = `postgres://localhost:5432/${databaseName}`)
+  : (DATABASE_URL = `${process.env.DATABASE_URL}?sslmode=require`);
 
+const db = new Sequelize(DATABASE_URL, {
+  logging: false,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: process.env.DATABASE_URL ? true : false,
+  },
+});
+module.exports = db;
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: false
-  }
-)
-module.exports = db
+if (process.env.NODE_ENV === "test") {
+  after("close database connection", () => db.close());
+}
